eb53f106d1ddd9bf62276b10b043e791ca404cad
CHAFAI802 <chafaimab@gmail.com>
2025-07-27 19:43:37 +0100

    Initial clean commit with django project models and admin not yet created

---
+++ test/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .  import views
+
+urlpatterns = [
+    path('',views.get_index,name='index'),
+    path('about',views.get_about,name='about'),
+    path('schedule',views.get_schedule,name='schedule'),
+    path('speakers',views.get_speakers,name='speakers'),
+    path ('venue',views.get_venue, name='venue'),
+    path('speakersdetails',views.get_speakers_details,name='speakerdetails'),
+    path('tickets',views.get_tickets,name='tickets'),
+    path('buy-tickets',views.get_buy_tickets,name='buy-tickets'),
+    path('gallery',views.get_gallery,name='gallery'),
+    path('terms',views.get_terms,name='terms'),
+    path('privacy',views.get_privacy,name='privacy'),
+    path('404',views.get_four_o_four,name='404'),
+    path('contact',views.get_contact,name='contact'),
+    
+]
---
+++ test/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def get_index(request):
+    return render (request,'parts/index.html')
+
+def get_about(request):
+    return render (request,'pages/about.html')
+
+def get_schedule(request):
+    return render (request ,'pages/schedule.html')
+
+def get_speakers(request):
+    return render (request ,'pages/speakers.html')
+
+def get_venue(request):
+    return render (request ,'pages/venue.html')
+
+def get_speakers_details(request):
+    return render (request ,'pages/speaker-details.html')
+
+def get_tickets(request):
+    return render (request ,'pages/tickets.html')
+
+def get_buy_tickets(request):
+    return render (request ,'pages/buy-tickets.html')
+
+def get_gallery(request):
+    return render (request ,'pages/gallery.html')
+
+def get_terms(request):
+    return render (request ,'pages/terms.html')
+
+def get_privacy(request):
+    return render (request ,'pages/privacy.html')
+
+def get_four_o_four(request):
+    return render (request ,'pages/404.html')
+
+def get_contact(request):
+    return render (request ,'pages/contact.html')
