dd1e6980db2184277de4d1508d5168d4768a0248
CHAFAI802 <chafaimab@gmail.com>
2025-07-28 14:30:41 +0100

    setting static and media files for dev and production mode

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=True,cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -122,14 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join (BASE_DIR,'static')
+STATIC_ROOT = os.path.join (BASE_DIR,'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'project/static')
+    os.path.join(BASE_DIR,'static')
 ]
-
+#-----------------------------
+# MEDIA FILES (uploads)
+#-----------------------------
+MEDIA_URL ='/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
